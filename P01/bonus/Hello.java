9e12b8811180272781676882c48dde09a80fae56
Digbijay Pandit <digbijaypandit46@gmail.com>
2025-08-21 14:16:41 -0500

    commit bonus

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
 public class Hello {
-  
+  public static void main(String[] args) {
+    Scanner rScanner = new Scanner(System.in);
+    System.out.println("Please print your name");
+    String name = rScanner.nextLine();
+    System.out.println("Hello "+name);
+    rScanner.close();
+  }
 }
