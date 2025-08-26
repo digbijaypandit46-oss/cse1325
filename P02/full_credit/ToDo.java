04d316182fd4944416815b2e2ebd91e657c7be4e
Digbijay Pandit <digbijaypandit46@gmail.com>
2025-08-26 11:59:30 -0500

    commit 1: this is the framework for the struct Item

---
+++ P02/full_credit/ToDo.java
@@ -0,0 +1,14 @@
+public class ToDo {
+  static class Item{
+    String task;
+    int priority;
+
+    Item(String task, int priority){
+      this.task = task;
+      this.priority = priority;
+    }
+  }
+  public static void main(String[] args) {
+    
+  }
+}
