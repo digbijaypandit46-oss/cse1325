5fb500e5ecc71c76579ae90bc71c86e049004ede
Digbijay Pandit <digbijaypandit46@gmail.com>
2025-09-28 18:48:54 -0500

    commit final: Rating part2

---
+++ P05/Rating.java
@@ -0,0 +1,35 @@
+public class Rating {
+  private int stars;
+  private Comment review;
+
+  Rating(int stars, Comment review){
+    this.stars = stars;
+    this.review = review;
+    if(stars>5||stars<1){
+      throw new IllegalArgumentException("must give a rating between 1-5 inclusive");
+    }
+  }
+  public int getStars(){
+    return stars;
+  }
+  public Comment getReview(){
+    return review;
+  }
+  @Override
+  public String toString(){
+    int star = 2605;
+    char s = (char) star;
+    int noStar = 2606;
+    char nS = (char) noStar;
+    StringBuilder sb = new StringBuilder();
+    sb.append("For "+stars+"-star: ");
+    for (int i = 0; i < 5; i++) {
+      if(i<=stars){
+        sb.append(s);
+      }else{
+        sb.append(nS);
+      }
+    }
+    return sb.toString();
+  }
+}
