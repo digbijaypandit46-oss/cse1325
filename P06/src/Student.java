e859c14bc1bc777a3353fcd432542e4cbdfc0116
Digbijay Pandit <digbijaypandit46@gmail.com>
2025-10-04 13:39:40 -0500

    P06 commit idk: Housekeeping

--- P06/src/Student.java
+++ P06/src/Student.java
@@ -2,9 +2,23 @@ import java.util.ArrayList;
 
 public class Student extends Person{
   private static int nextStudentID = 0;
-  private int StudentID;
+  private int studentID;
   private ArrayList<Course> courses;
-  Student(){
+  public Student(String name, String email){
     super(name, email);
+    this.studentID = nextStudentID;
+    nextStudentID++;
+    this.courses = new ArrayList<>();
+  }
+  public void addCourse(Course course){
+    courses.add(course);
+  }
+  public Course[] getCourses() {
+    return courses.toArray(new Course[courses.size()]);
+  }
+  @Override
+  public String toString(){
+    String superString = super.toString();
+    return superString.replace(")", ", #" + studentID + ")");
   }
 }
