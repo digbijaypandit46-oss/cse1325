6b14b85f505b45b02bb6a2946fdd45c375160f96
Digbijay Pandit <digbijaypandit46@gmail.com>
2025-10-09 10:57:56 -0500

    P07 commit 3: Session java is actually finished now (not considering possible bug fixes)

--- P07/src/session/Session.java
+++ P07/src/session/Session.java
@@ -12,9 +12,26 @@ public class Session {
   Session(Course course, Tutor tutor){
     this.course =course;
     this.tutor = tutor;
+    this.students = new ArrayList<Student>();
   }
 
   public void setSchedule(String date, String startTime,long duration ){
-    
+    this.dates = new DateRange(date, startTime, duration);
+  }
+
+  public void addStudent(Student student){
+    students.add(student);
+  }
+
+  @Override
+  public String toString(){
+    StringBuilder sb = new StringBuilder("Session on "+course.toString()+" at");
+    sb.append(dates.toString());
+    sb.append("\nTutor: "+tutor.toString()+"\n Students: ");
+    for (Student student : students) {
+      sb.append(" "+student.toString()+" ");
+    }
+
+    return sb.toString();
   }
 }
